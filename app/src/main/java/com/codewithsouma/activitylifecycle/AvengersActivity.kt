9356dd06b2e4fcb60b83dbd7d191a732df852664
Souma <souma.hitech@gmail.com>
2021-03-06 19:38:52 +0530

    add menu in the app

--- app/src/main/java/com/codewithsouma/activitylifecycle/AvengersActivity.kt
+++ app/src/main/java/com/codewithsouma/activitylifecycle/AvengersActivity.kt
@@ -1,17 +1,46 @@
 package com.codewithsouma.activitylifecycle
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class AvengersActivity : AppCompatActivity() {
     var titleName:String? = "Avengers"
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.acitvity_avengers)
-        if (intent != null){
-            titleName = intent.getStringExtra("nameOfAvenger")
-        }
 
-        title = titleName
+        sharedPreferences = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+        title = sharedPreferences.getString("title","The Avengers")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.avengers_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.logout -> {
+                sharedPreferences.edit().clear().apply()
+                val intent = Intent(this@AvengersActivity,LoginActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+
+            R.id.exit -> {
+                finish()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/codewithsouma/activitylifecycle/LoginActivity.kt
+++ app/src/main/java/com/codewithsouma/activitylifecycle/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.codewithsouma.activitylifecycle
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -16,10 +18,21 @@ class LoginActivity : AppCompatActivity() {
     lateinit var txtRegister: TextView
     private val validMobileNumber = "8768454982"
     private val validPassword = arrayOf("tony","steve","bruce","thanos","Admin")
+    lateinit var sharedPreference : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        sharedPreference = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+        val isLoggedIn = sharedPreference.getBoolean("isLoggedIn",false)
+        if (isLoggedIn){
+            val intent = Intent(this@LoginActivity,AvengersActivity::class.java)
+
+            startActivity(intent)
+            finish()
+        }
+
 
         title = "Log In"
 
@@ -44,27 +57,27 @@ class LoginActivity : AppCompatActivity() {
             when (password) {
                 validPassword[0] -> {
                     nameOfAvenger = "Iron Man"
-                    intent.putExtra("nameOfAvenger", nameOfAvenger)
+                    savePreference(nameOfAvenger)
                     startActivity(intent)
                 }
                 validPassword[1] -> {
                     nameOfAvenger = "Captain America"
-                    intent.putExtra("nameOfAvenger", nameOfAvenger)
+                    savePreference(nameOfAvenger)
                     startActivity(intent)
                 }
                 validPassword[2] -> {
                     nameOfAvenger = "The Hulk"
-                    intent.putExtra("nameOfAvenger", nameOfAvenger)
+                    savePreference(nameOfAvenger)
                     startActivity(intent)
                 }
                 validPassword[3] -> {
                     nameOfAvenger = "The Avengers"
-                    intent.putExtra("nameOfAvenger", nameOfAvenger)
+                    savePreference(nameOfAvenger)
                     startActivity(intent)
                 }
                 validPassword[4] -> {
                     nameOfAvenger = "Admin"
-                    intent.putExtra("nameOfAvenger", nameOfAvenger)
+                    savePreference(nameOfAvenger)
                     startActivity(intent)
                 }
                 else -> Toast.makeText(this@LoginActivity, "Incorrect Credentials", Toast.LENGTH_LONG).show()
@@ -76,5 +89,10 @@ class LoginActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         finish()
+    }
+
+    private fun savePreference(title:String){
+        sharedPreference.edit().putBoolean("isLoggedIn",true).apply()
+        sharedPreference.edit().putString("title",title).apply()
     }
 }
